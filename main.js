d947f7cc07251701209c17d3cc8dea9572003183
Suraj Kumar <surajkumar@Krishnas-MacBook-Air.local>
2024-01-15 18:29:30 +0530

    fix: update format

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-$(function() {
-    $(window).on("scroll", function() {
-        if($(window).scrollTop() > 50) {
-            $(".header a:hover").addClass("active");
-        } else {
-            //remove the background property so it comes transparent again (defined in your css)
-           $(".header a:hover").removeClass("active");
-        }
-    });
+$(function () {
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 50) {
+      $(".header a:hover").addClass("active");
+    } else {
+      //remove the background property so it comes transparent again (defined in your css)
+      $(".header a:hover").removeClass("active");
+    }
+  });
 });
